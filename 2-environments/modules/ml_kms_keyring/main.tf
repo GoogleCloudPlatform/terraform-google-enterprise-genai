ea8090b5e4de9b6bd665bb2ff368bdbf7f8ce6b8
Caetano Colin <164910343+caetano-colin@users.noreply.github.com>
2024-05-08 08:45:35 -0300

    Update 2-environments/modules/ml_kms_keyring/main.tf
    Co-authored-by: Daniel Andrade <dandrade@ciandt.com>

--- 2-environments/modules/ml_kms_keyring/main.tf
+++ 2-environments/modules/ml_kms_keyring/main.tf
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 module "kms_keyrings" {
   for_each = toset(var.keyring_regions)
   source   = "terraform-google-modules/kms/google"
