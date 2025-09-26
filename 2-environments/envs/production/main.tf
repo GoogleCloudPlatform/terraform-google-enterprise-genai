89583711de25eb959fbc9a93dbe23555fc5f57b7
mariamartins <mariamartins@ciandt.com>
2025-09-26 10:29:56 -0300

    fix lint

--- 2-environments/envs/production/main.tf
+++ 2-environments/envs/production/main.tf
@@ -23,7 +23,7 @@ module "env" {
   remote_state_bucket        = var.remote_state_bucket
   tfc_org_name               = var.tfc_org_name
 
-  kms_prevent_destroy        = var.kms_prevent_destroy
+  kms_prevent_destroy = var.kms_prevent_destroy
 
   assured_workload_configuration = {
     enabled           = false
