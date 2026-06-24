4b1c733e40d4ac082a07a181320c85a256812517
Maria Martins <mariamartins@ciandt.com>
2026-06-24 10:17:04 -0300

    add cloud dns not enabled retry error

--- test/integration/testutils/retry.go
+++ test/integration/testutils/retry.go
@@ -41,6 +41,9 @@ var (
 		// Error 403: Compute Engine API has not been used in project {} before or it is disabled.
 		".*Error 403.*Compute Engine API has not been used in project.*": "Compute Engine API not enabled",
 
+		// Error 403: Cloud DNS API has not been used in project {} before or it is disabled.
+		".*Error 403.*Cloud DNS API has not been used in project.*": "Cloud DNS API not enabled",
+
 		// Error 400: Service account {} does not exist.
 		".*Error 400.*Service account.*does not exist*": "Error setting IAM policy",
 
